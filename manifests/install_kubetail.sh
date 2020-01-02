fd725e1e7b0f1a0656eb067799210cac3bfa2794
sysnet4admin <pagaia@hotmail.com>
2020-01-02 16:03:29 +0900

    init kubetail installer

---
+++ manifests/install_kubetail.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env bash
+# usage: 
+# 1. bash <(curl -s https://raw.githubusercontent.com/sysnet4admin/IaC/master/manifests/install_kubetail.sh) 
+
+curl -O https://raw.githubusercontent.com/johanhaleby/kubetail/master/kubetail
+chmod 744 kubetail
+mv kubetail /sbin
+
+echo "example: "
+echo "1. kubetail -l component=speaker -n metallb-system"
+echo "2. kubetail -l k8s-app=calico-node -n kube-system"
+echo "3 kubetail <pod_name_as_default>"
+
