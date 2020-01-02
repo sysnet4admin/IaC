b648fdd983d77d9a780a476fccf23eb940001baa
sysnet4admin <pagaia@hotmail.com>
2020-01-02 16:06:32 +0900

    few fixed

--- manifests/install_kubetail.sh
+++ manifests/install_kubetail.sh
@@ -6,8 +6,8 @@ curl -O https://raw.githubusercontent.com/johanhaleby/kubetail/master/kubetail
 chmod 744 kubetail
 mv kubetail /sbin
 
-echo "example: "
+echo ""
+echo "Example: "
 echo "1. kubetail -l component=speaker -n metallb-system"
 echo "2. kubetail -l k8s-app=calico-node -n kube-system"
 echo "3 kubetail <pod_name_as_default>"
-
