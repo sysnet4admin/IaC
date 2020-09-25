f6892db2ebbb781d8ea5c0d7227c2b8c3b976baa
sysnet4admin <pagaia@hotmail.com>
2020-09-26 00:34:27 +0900

    mod unicode issue

--- Docker/Registry/create-registry.sh
+++ Docker/Registry/create-registry.sh
@@ -13,12 +13,12 @@ do
 done
 
 docker run -d \
-  --restart=always \
-  --name registry \
-  -v /root/IaC/Docker/Registry:/certs:ro \ 
-  -v /data:/var/lib/registry \
-  -e REGISTRY_HTTP_ADDR=0.0.0.0:443 \
-  -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/tls.crt \
-  -e REGISTRY_HTTP_TLS_KEY=/certs/tls.key \
-  -p 8443:443 \
-  registry:2
+  --restart=always \
+  --name registry \
+  -v /root/IaC/Docker/Registry:/certs:ro \ 
+  -v /data:/var/lib/registry \
+  -e REGISTRY_HTTP_ADDR=0.0.0.0:443 \
+  -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/tls.crt \
+  -e REGISTRY_HTTP_TLS_KEY=/certs/tls.key \
+  -p 8443:443 \
+  registry:2
