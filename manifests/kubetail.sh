0fbff0ae2926eafefb83bfc972b52d59265a5155
sysnet4admin <pagaia@hotmail.com>
2020-11-01 11:24:45 +0900

    add kubetail.sh

---
+++ manifests/kubetail.sh
@@ -0,0 +1,10 @@
+#Main_Source_From: 
+# - https://github.com/johanhaleby/kubetail
+
+# usage: 
+# 1. Create 
+# - bash <(curl -s  https://raw.githubusercontent.com/sysnet4admin/IaC/master/manifests/kubetail.sh) 
+
+curl -O https://raw.githubusercontent.com/johanhaleby/kubetail/master/kubetail
+chmod 755 kubetail
+mv ~/kubetail /usr/local/bin
