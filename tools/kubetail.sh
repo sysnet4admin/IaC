4e28061b32676abc3408849ce34e24c3073d53ca
sysnet4admin <pagaia@hotmail.com>
2021-01-01 13:46:46 +0900

    upt tools

--- tools/kubetail.sh
+++ tools/kubetail.sh
@@ -9,3 +9,8 @@ curl -O https://raw.githubusercontent.com/johanhaleby/kubetail/master/kubetail
 chmod 755 kubetail
 mv ~/kubetail /usr/local/bin
 echo "kubetail install successfully"
+echo ""
+echo "Example: "
+echo "1. kubetail -l component=speaker -n metallb-system"
+echo "2. kubetail -l k8s-app=calico-node -n kube-system"
+echo "3. kubetail <pod_name_as_default>"
