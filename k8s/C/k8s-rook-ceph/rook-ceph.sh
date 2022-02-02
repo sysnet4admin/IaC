5618475d8588dd8ded00d718542949f7053d12fc
Hoon Jo <pagaia@hotmail.com>
2022-02-02 14:53:26 +0900

    upt rook-ceph.sh

--- k8s/C/k8s-rook-ceph/rook-ceph.sh
+++ k8s/C/k8s-rook-ceph/rook-ceph.sh
@@ -6,18 +6,21 @@ cd $HOME ; git clone --single-branch --branch v1.8.3 https://github.com/rook/roo
 
 cat <<EOF > $HOME/rook-ceph-installer.sh
 cd $HOME/rook/deploy/examples
-kubectl creeate -f crds.yaml -f common.yaml -f operator.yaml
+kubectl create -f crds.yaml -f common.yaml -f operator.yaml
 
-echo "clsuter will be installed"
+echo -e "\nclsuter will be installed"
 sleep 3 ; kubectl create -f cluster.yaml
 
-echo "ceph-toolbox will be installed"
+echo -e "\nceph-toolbox will be installed"
 # https://rook.io/docs/rook/v1.8/ceph-toolbox.html
 sleep 6 kubectl create -f toolbox.yaml
 
 # Check properly on 
-while [ "kubectl get pod -n rook-ceph | wc -l" <= 33 ]; do
-    echo "still in deploying cluster"
+TOTAL_RC=\$(kubectl get pod -n rook-ceph | tail -n +2 | wc -l)
+while [ \$TOTAL_RC -le 33 ]; do
+    TOTAL_RC=\$(kubectl get pod -n rook-ceph | tail -n +2 | wc -l)
+    echo "still in deploying cluster \$TOTAL_RC/33"
     sleep 30 
 done
+   echo "\nSuccessfully deployed rook-ceph cluster \$TOTAL_RC/33"
 EOF
