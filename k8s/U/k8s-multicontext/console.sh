c061b03c299c4269f6d068f2fae4a28a1fca0a3b
Hoon Jo <pagaia@hotmail.com>
2022-02-02 13:21:33 +0900

    upt multicontext

--- k8s/U/k8s-multicontext/console.sh
+++ k8s/U/k8s-multicontext/console.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# vagrant_home_k8s_config 
-va_k8s_cfg="/home/vagrant/.kube/config" 
+# root_home_k8s_config 
+va_k8s_cfg="/root/.kube/config" 
 
 # install util packages 
 apt install sshpass
@@ -23,26 +23,39 @@ apt install kubectl=$1
 kubectl completion bash >/etc/bash_completion.d/kubectl
 
 # alias kubectl to k 
-echo 'alias k=kubectl' >> /home/vagrant/.bashrc
+echo 'alias k=kubectl' >> ~/.bashrc
 echo "alias ka='kubectl apply -f'" >> ~/.bashrc
 echo "alias kd='kubectl delete -f'" >> ~/.bashrc
-echo 'complete -F __start_kubectl k' >> /home/vagrant/.bashrc
+echo 'complete -F __start_kubectl k' >> ~/.bashrc
 
 # create .kube_config dir
-sudo -u vagrant mkdir /home/vagrant/.kube
+mkdir /root/.kube
 
 # copy hosts file by sshpass
-sudo -u vagrant sshpass -p 'vagrant' scp -o StrictHostKeyChecking=no root@192.168.1.$2:/etc/hosts /tmp/$2-hosts
-sudo -u vagrant sshpass -p 'vagrant' scp -o StrictHostKeyChecking=no root@192.168.1.$3:/etc/hosts /tmp/$3-hosts
-sudo -u vagrant sshpass -p 'vagrant' scp -o StrictHostKeyChecking=no root@192.168.1.$4:/etc/hosts /tmp/$4-hosts
+sshpass -p 'vagrant' scp -o StrictHostKeyChecking=no root@192.168.1.$2:/etc/hosts /tmp/$2-hosts
+sshpass -p 'vagrant' scp -o StrictHostKeyChecking=no root@192.168.1.$3:/etc/hosts /tmp/$3-hosts
+sshpass -p 'vagrant' scp -o StrictHostKeyChecking=no root@192.168.1.$4:/etc/hosts /tmp/$4-hosts
 cat /tmp/$2-hosts >> /etc/hosts; cat /tmp/$3-hosts >> /etc/hosts; cat /tmp/$4-hosts >> /etc/hosts
 
 # copy kubeconfig by sshpass
-sudo -u vagrant sshpass -p 'vagrant' scp -o StrictHostKeyChecking=no root@192.168.1.$2:/root/.kube/config $va_k8s_cfg-$2
-sudo -u vagrant sshpass -p 'vagrant' scp -o StrictHostKeyChecking=no root@192.168.1.$3:/root/.kube/config $va_k8s_cfg-$3
-sudo -u vagrant sshpass -p 'vagrant' scp -o StrictHostKeyChecking=no root@192.168.1.$4:/root/.kube/config $va_k8s_cfg-$4
+sshpass -p 'vagrant' scp -o StrictHostKeyChecking=no root@192.168.1.$2:/root/.kube/config $va_k8s_cfg-$2
+sshpass -p 'vagrant' scp -o StrictHostKeyChecking=no root@192.168.1.$3:/root/.kube/config $va_k8s_cfg-$3
+sshpass -p 'vagrant' scp -o StrictHostKeyChecking=no root@192.168.1.$4:/root/.kube/config $va_k8s_cfg-$4
 
 # flatten .kube_config 
 export KUBECONFIG=$va_k8s_cfg-$2:$va_k8s_cfg-$3:$va_k8s_cfg-$4
 kubectl config view --flatten > $va_k8s_cfg 
-chown vagrant:vagrant .kube/config
+
+# git clone k8s-code
+git clone https://github.com/sysnet4admin/_Lecture_k8s_learning.kit.git
+mv /home/vagrant/_Lecture_k8s_learning.kit $HOME
+find $HOME/_Lecture_k8s_learning.kit -regex ".*\.\(sh\)" -exec chmod 700 {} \;
+
+# make rerepo-k8s-learning.kit and put permission
+cat <<EOF > /usr/local/bin/rerepo-k8s-learning.kit
+#!/usr/bin/env bash
+rm -rf $HOME/_Lecture_k8s_learning.kit
+git clone https://github.com/sysnet4admin/_Lecture_k8s_learning.kit.git $HOME/_Lecture_k8s_learning.kit
+find $HOME/_Lecture_k8s_learning.kit -regex ".*\.\(sh\)" -exec chmod 700 {} \;
+EOF
+chmod 700 /usr/local/bin/rerepo-k8s-learning.kit
