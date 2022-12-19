4beaee8318a2a5af32f8b7cdd4554a304cdace1b
Hoon Jo <pagaia@hotmail.com>
2022-12-19 11:09:54 +0900

    fixed for ubuntu 22.04

--- k8s/k8s-console/k8s_pkg_cfg.sh
+++ k8s/k8s-console/k8s_pkg_cfg.sh
@@ -9,6 +9,7 @@ apt-get install sshpass
 
 # add kubernetes repo
 apt-get update && apt-get install -y apt-transport-https ca-certificates curl
+
 curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" \
   | sudo tee /etc/apt/sources.list.d/kubernetes.list
@@ -18,6 +19,14 @@ apt-get update
 # install kubectl and fixed ver
 apt-get install -y kubectl=$1 
 apt-mark hold kubelet
+
+# add docker-ce repo 
+apt-get install -y gnupg lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # install & enable docker 
 apt-get install -y docker-ce=$2 docker-ce-cli=$2 
