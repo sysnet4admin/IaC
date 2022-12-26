aaf0bb9b6e9c0dddd4ceb11f0862defc800a9447
Hoon Jo <pagaia@hotmail.com>
2022-12-26 12:44:50 +0900

    fixed prom

--- Prometheus/demo-prom-darksite/prom.sh
+++ Prometheus/demo-prom-darksite/prom.sh
@@ -3,6 +3,9 @@
 # https://blog.marcnuri.com/prometheus-grafana-setup-minikube
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm install prometheus prometheus-community/prometheus
-kubectl expose service prometheus-server --type=NodePort --target-port=9090 --name=prometheus-server-np
 
+kubectl expose service prometheus-server --type=NodePort --target-port=9090 --name=prometheus-server-np
 minikube service prometheus-server-np
+kubectl patch service prometheus-server-np --namespace=default \
+   --type='json' \
+   --patch='[{"op": "replace", "path": "/spec/ports/0/nodePort", "value":30000}]'
