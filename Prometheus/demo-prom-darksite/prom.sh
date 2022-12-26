db6df8e115bb815c2c70133e7ca9a9413b4f6a08
Hoon Jo <pagaia@hotmail.com>
2022-12-26 11:13:57 +0900

    demo-prom-darksite

---
+++ Prometheus/demo-prom-darksite/prom.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# https://blog.marcnuri.com/prometheus-grafana-setup-minikube
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm install prometheus prometheus-community/prometheus
+kubectl expose service prometheus-server --type=NodePort --target-port=9090 --name=prometheus-server-np
+
+minikube service prometheus-server-np
