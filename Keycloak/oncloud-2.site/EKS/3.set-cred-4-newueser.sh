25ff3c273a1679a77ce11ef3fe16bd19548d7243
Hoon Jo <pagaia@hotmail.com>
2023-10-28 09:04:26 +0900

    chg oidc user name

--- Keycloak/oncloud-2.site/EKS/3.set-cred-4-newueser.sh
+++ Keycloak/oncloud-2.site/EKS/3.set-cred-4-newueser.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-kubectl config set-credentials oidc \
+kubectl config set-credentials newuser \
   --exec-api-version=client.authentication.k8s.io/v1beta1 \
   --exec-command=kubelogin
