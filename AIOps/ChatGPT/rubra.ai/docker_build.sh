d989088510a49c752c19cd3bacc3aed67314bb75
Hoon Jo <pagaia@hotmail.com>
2024-02-15 16:29:18 +0900

    upt rubra.ai

--- AIOps/ChatGPT/rubra.ai/docker_build.sh
+++ AIOps/ChatGPT/rubra.ai/docker_build.sh
@@ -1,6 +1,26 @@
 #!/usr/bin/env bash
 
+# avoid 'dpkg-reconfigure: unable to re-open stdin: No file or directory'
+export DEBIAN_FRONTEND=noninteractive
+
+# add docker-ce repo with containerd
+apt-get update && apt-get install gnupg lsb-release
+curl -fsSL \
+  https://download.docker.com/linux/ubuntu/gpg \
+  | gpg --dearmor -o /etc/apt/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) \
+  signed-by=/etc/apt/keyrings/docker-archive-keyring.gpg] \
+  https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" \
+  | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
 # install & enable docker
 apt-get update
 apt-get install docker-ce=$1 docker-ce-cli=$1 -y
 systemctl enable --now docker
+
+# install docker-compose 
+curl -L https://github.com/docker/compose/releases/download/v2.24.4/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+chmod 744 /usr/local/bin/docker-compose
+
