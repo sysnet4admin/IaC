1caa35502aaee82cfaebfbf25e1a0b7b2d9e6ef5
Hoon Jo <pagaia@hotmail.com>
2024-02-21 17:28:57 +0900

    upt scripts

--- AIOps/ChatGPT/rubra.ai/run-rubra.ai.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# avoid 'dpkg-reconfigure: unable to re-open stdin: No file or directory'
-export DEBIAN_FRONTEND=noninteractive
-
-curl -sfL https://get.rubra.ai | bash -s -- start
-
-
