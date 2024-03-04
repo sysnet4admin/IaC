baf9ef23a45326cf937aa3c942a94308bdc2eab6
Hoon Jo <pagaia@hotmail.com>
2024-03-04 13:06:14 +0900

    expand disk size

---
+++ AIOps/K8sGPT/LocalAI/hugging_face.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# init for hugging face 
+pip install huggingface_hub
+apt-get install git-lfs -y 
+
+# ssh key gen 
+ssh-keygen -t ed25519 -C "pagaia@hotmail.com"
+
+# https://huggingface.co/docs/hub/security-git-ssh#add-a-ssh-key-to-your-account
+# cat ~/.ssh/id_ed25519.pub
+# ssh -T git@hf.co 
+
+
+
