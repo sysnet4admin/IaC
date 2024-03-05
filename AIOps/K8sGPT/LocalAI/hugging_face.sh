1fc97bbd6178ffa0bb98ed8eef57e7bd968c3e46
Hoon Jo <pagaia@hotmail.com>
2024-03-05 14:46:29 +0900

    chg api-port

--- AIOps/K8sGPT/LocalAI/hugging_face.sh
+++ AIOps/K8sGPT/LocalAI/hugging_face.sh
@@ -36,11 +36,10 @@ pip install -r extensions/openai/requirements.txt
 $HOME/text-generation-webui/start_linux.sh \
   --listen --listen-host 0.0.0.0 --listen-port=7861 \
   --extenstions openai --model TinyLlama_TinyLlama-1.1B-Chat-v1.0 \
-  --api-port 5001
-<<EOF
+  --api-port 5001 <<EOF & 
 N
-EOF &
-echo "Open: 0.0.0.0:7861"
+EOF 
+echo "Web: 0.0.0.0:7861, API: 0.0.0.0:5001"
 
 # N = NO GPU
 
