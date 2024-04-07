5090dcc42ea5b464a074d5ac860b3d08ee5aba1e
Hoon Jo <pagaia@hotmail.com>
2024-04-07 14:30:37 +0900

    upt nfs_exporter for multi-platform

--- k8s/extra-pkgs/v1.30/nfs_exporter.sh
+++ k8s/extra-pkgs/v1.30/nfs_exporter.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
-nfsdir=/nfs_shared/$1
+NFS_DIR=/nfs_shared/$1
 
+# convert term in CentOS and Ubuntu distro linux  
+if [[ "$(awk -F '=' '/PRETTY_NAME/ { print $2 }' /etc/os-release)" = *"CentOS"* ]]; then 
+  NFS_SVC_NAME="nfs"
+elif [[ "$(awk -F '=' '/PRETTY_NAME/ { print $2 }' /etc/os-release)" = *"Ubuntu"* ]]; then
+  NFS_SVC_NAME="nfs-server"  
+else
+  echo "This system is not CentOS as well as Ubuntu"
+fi 
+
+# main nfs-exporter
 if [ $# -eq 0 ]; then
   echo "usage: nfs-exporter.sh <name>"; exit 0
 fi
@@ -9,11 +19,20 @@ if [[ ! -d /nfs_shared ]]; then
   mkdir /nfs_shared
 fi
 
-if [[ ! -d $nfsdir ]]; then
-  mkdir -p $nfsdir
-  echo "$nfsdir 192.168.1.0/24(rw,sync,no_root_squash)" >> /etc/exports
-  if [[ $(systemctl is-enabled nfs) -eq "disabled" ]]; then
-    systemctl enable nfs
+if [[ ! -d $NFS_DIR ]]; then
+  mkdir -p $NFS_DIR
+  echo "$NFS_DIR 192.168.1.0/24(rw,sync,no_root_squash)" >> /etc/exports
+  if [[ $(systemctl is-enabled "$NFS_SVC_NAME") -eq "disabled" ]]; then
+    systemctl enable $NFS_SVC_NAME
   fi
-    systemctl restart nfs
+    systemctl restart $NFS_SVC_NAME
 fi
+
+# check purpose
+echo -e "Check created configurations"
+echo -e "==="
+echo -e "cat /etc/exports:"
+echo -e "$(cat /etc/exports)" | ( DASH=$'  - ' ; sed "s/^/$DASH/" )
+echo -e "---"
+echo -e "ls /nfs_shared:"
+echo -e "$(ls /nfs_shared/)" | ( DASH=$'  - ' ; sed "s/^/$DASH/" )
