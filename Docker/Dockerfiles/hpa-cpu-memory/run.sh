2b45945693b0b6b9f1b3f691a359f85ffb084218
Hoon Jo <pagaia@hotmail.com>
2024-05-30 14:19:24 +0900

    upt hpa-cpu-memory

--- Docker/Dockerfiles/hpa-cpu-memory/run.sh
+++ Docker/Dockerfiles/hpa-cpu-memory/run.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
-spawn-fcgi -p 9001 -P /run/spawn-fcgi.pid -- /usr/sbin/fcgiwrap -c 3;
+#!/bin/sh
+spawn-fcgi -p 9001 -P /run/spawn-fcgi.pid -- /usr/bin/fcgiwrap -c 3;
 ./docker-entrypoint.sh nginx;
